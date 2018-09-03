4f3aa3e977532a5dc6ef1461a73b5f99011958c2
Tiimor <tiimor@qq.com>
2018-09-03 14:36:12 +0800

    优化示例

--- sample/src/main/java/com/excellence/ffmpeg/sample/MainActivity.java
+++ sample/src/main/java/com/excellence/ffmpeg/sample/MainActivity.java
@@ -7,15 +7,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.excellence.exec.CommandTask;
 import com.excellence.exec.IListener;
 import com.excellence.ffmpeg.FFmpeg;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements IListener {
 
     public static final String TAG = MainActivity.class.getSimpleName();
 
     private Button mButton = null;
     private TextView mTextView = null;
+    private CommandTask mTask = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,33 +33,40 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mTextView.setText("");
+                if (mTask != null) {
+                    mTask.discard();
+                    mTask = null;
+                    return;
+                }
                 String cmd = "-version";
-                FFmpeg.addTask(cmd, new IListener() {
-                    @Override
-                    public void onPre(String command) {
-                        Log.i(TAG, "onPre: " + command);
-                        mTextView.append(command + "\n");
-                    }
-
-                    @Override
-                    public void onProgress(String message) {
-                        Log.i(TAG, "onProgress: " + message);
-                        mTextView.append(message + "\n");
-                    }
-
-                    @Override
-                    public void onError(Throwable t) {
-                        t.printStackTrace();
-                        mTextView.setText("Error:" + t.getMessage());
-                    }
-
-                    @Override
-                    public void onSuccess(String message) {
-                        Log.i(TAG, "onSuccess: " + message);
-                        mTextView.append(message + "\n");
-                    }
-                });
+                // mTask = FFmpeg.addTask(cmd, MainActivity.this);
+                mTask = new CommandTask.Builder().command(FFmpeg.checkFFmpeg()).command(cmd).build();
+                mTask.deploy(MainActivity.this);
             }
         });
+    }
+
+    @Override
+    public void onPre(String command) {
+        Log.i(TAG, "onPre: " + command);
+        mTextView.append(command + "\n");
+    }
+
+    @Override
+    public void onProgress(String message) {
+        Log.i(TAG, "onProgress: " + message);
+        mTextView.append(message + "\n");
+    }
+
+    @Override
+    public void onError(Throwable t) {
+        t.printStackTrace();
+        mTextView.setText("Error:" + t.getMessage());
+    }
+
+    @Override
+    public void onSuccess(String message) {
+        Log.i(TAG, "onSuccess: " + message);
+        mTextView.append(message + "\n");
     }
 }
